b72e4f19a692feb6e2e7226537e88c183fc017d1
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-06-13 15:24:54 +1000

    tried to fetch data

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
-const ReposPage = () => {
-  return (
-    <div>ReposPage</div>
-  )
-}
-
-export default ReposPage
+import Link from 'next/link';
+async function getData() {
+    const res = await fetch('https://api.github.com/users/nddung2812/repos',
+        {
+            next: {
+                revalidate: 60,
+            },
+        }
+    )
+    
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+   
+    return res.json()
+  }
+  export default async function ReposPage() {
+    const data = await getData();
+    console.log(data)
+    return (
+        <div className='repos-container'>
+        <h2>test</h2>
+        </div>
+    );
+};
